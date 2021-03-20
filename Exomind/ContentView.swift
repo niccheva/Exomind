03c355debe14fb7b0df85aed4130198927ef5c46
Nicolas Chevalier <nchevalier.atos@lfdj.com>
2021-03-20 16:20:51 +0100

    Initial Commit

---
+++ Exomind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exomind
+//
+//  Created by Nicolas Chevalier on 20/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
